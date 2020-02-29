41c6be1bf61045ef7c802c111b72555ad0429b90
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-29 00:28:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken(board, index)
+  index=input_to_index(user_input)
+
+if user_input.to_i == 2
+  return user_input.to_i - 1 = "x"
+elsif the position is taken == !"  "
+  return "false"
+elsif the position is not taken == user_input
+  return "true"
+else enter edge case == " "
+  return "true"
+end        # code your #position_taken? method here!
