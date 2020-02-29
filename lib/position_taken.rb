279241e6009563d58c5e66444edd36cad711636a
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-29 03:36:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ def position_taken?(board, index)
 space = "  "
 if space == "X" || "O"
   return false
-else index == "X" || "O"
+else index.to_i == "X" || "O"
   return true
   end      # code your #position_taken? method here!
 end
