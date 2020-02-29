d702de2e7ca149ae0d062421e701d283fd832b84
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-02-29 00:29:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ def position_taken(board, index)
   index=input_to_index(user_input)
 
 if user_input.to_i == 2
-  return user_input.to_i - 1 = "x"
+  return user_input.to_i - 1 == "x"
 elsif the position is taken == !"  "
   return "false"
 elsif the position is not taken == user_input
